bbec4cd115a2bbe9a07447bdd68b982f38f30203
Damian Velasquez <dmngit.code@gmail.com>
2021-11-01 12:01:27 -0300

    Terminamos de agregar los botones al slider, falta agregarles funcionalidad

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,14 @@
 const btnMenu = document.getElementById('btn-menu');
 const menu = document.getElementById('menu');
 
+const menuSlider = document.getElementById('menu-slider');
+menuSlider.addEventListener('click', (e) => {
+    console.dir(e.target)
+})
+
+
+
+
 
 btnMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-visible')
