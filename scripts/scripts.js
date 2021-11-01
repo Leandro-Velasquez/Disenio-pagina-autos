01219a9e7c10426fa2dd24b71ae3108a5b97e3bb
Damian Velasquez <dmngit.code@gmail.com>
2021-11-01 00:40:31 -0300

    Terminamos el menu del header

---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+const btnMenu = document.getElementById('btn-menu');
+const menu = document.getElementById('menu');
+btnMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu-visible')
+})
